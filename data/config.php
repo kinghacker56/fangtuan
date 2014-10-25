96676aa125c3a46bd7e3bc53c65df38704cda252
kinghacker56 <gsupercode@gmail.com>
2014-10-25 11:25:56 +0800

    初始
    开启新的旅程

---
+++ data/config.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "fangtuan";
+
+// database username
+$db_user   = "dev";
+
+// database password
+$db_pass   = "dev";
+
+// table prefix
+$prefix    = "ecsft_";
+
+$timezone    = "Asia/Shanghai";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2014-10-25 10:11:46');
+
+?>
